3a928b49cbb6905e7d0eb0b400822c9ff135a86b
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2011-08-20 09:40:36 -0300

    adicionado ao validação unica de codigo o scopodo grupo

--- app/models/taxe/type.rb
+++ app/models/taxe/type.rb
@@ -1,6 +1,6 @@
 class Taxe::Type < ActiveRecord::Base
   has_many :Values, :class_name => "Taxe::Values"
   belongs_to :Group, :class_name => "Taxe::Group"
-  validates_uniqueness_of :code
+  validates_uniqueness_of :code, :scope => :taxe_group_id
 end
 
