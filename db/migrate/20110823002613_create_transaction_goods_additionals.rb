bac9c610b0de87f291b0a624307dcca065c65bcf
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2011-08-24 23:54:13 -0300

    corrigido nome da coluna estava errada

--- db/migrate/20110823002613_create_transaction_goods_additionals.rb
+++ db/migrate/20110823002613_create_transaction_goods_additionals.rb
@@ -2,7 +2,7 @@ class CreateTransactionGoodsAdditionals < ActiveRecord::Migration
   def self.up
     create_table :transaction_goods_additionals do |t|
       t.integer :transaction_goods_item_id, :null => false
-      t.integer :goods_additional_id, :null => false
+      t.integer :goods_additional_value_id, :null => false
       t.boolean :active, :null => false, :default => true
       t.text :remark
 
