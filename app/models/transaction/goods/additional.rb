02573fc66c0ec0236a8c2e7391f8d650aad9af16
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2011-08-31 23:23:21 -0300

    valor adicional deve ser unico no item e no total

--- app/models/transaction/goods/additional.rb
+++ app/models/transaction/goods/additional.rb
@@ -4,4 +4,5 @@ class Transaction::Goods::Additional < ActiveRecord::Base
   validates_presence_of :transaction_goods_item_id, :goods_additional_value_id
   validates_associated :Item, :class_name => "Transaction::Goods::Item"
   validates_associated :Value, :class_name => "Goods::Item::Additional::Value"
+  validates_uniqueness_of :goods_additional_value_id
 end
--- app/models/transaction/tot.rb
+++ app/models/transaction/tot.rb
@@ -4,4 +4,5 @@ class Transaction::Tot < ActiveRecord::Base
   validates_presence_of :transaction_record_id, :goods_additional_value_id
   validates_associated :Record, :class_name => "Transaction::Record"
   validates_associated :Value, :class_name => "Goods::Item::Additional::Value"
+  validates_uniqueness_of :goods_additional_value_id
 end
