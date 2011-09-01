717396400e1b638d2a74843def9bba5d933d37c4
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2011-08-31 23:26:28 -0300

    id do financeiro deve ser unico no financeiro do registro

--- app/models/transaction/financier.rb
+++ app/models/transaction/financier.rb
@@ -3,4 +3,5 @@ class Transaction::Financier < ActiveRecord::Base
 	belongs_to :Record, :class_name => "Transaction::Record::Record"
 	validates_presence_of :transaction_record_id
 	validates_presence_of :financier_note_id
+  validates_uniqueness_of :financier_note_id
 end
