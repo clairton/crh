fb4fea74d1f549b4e4b09f5e213488aa198f0738
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2011-08-31 23:18:00 -0300

    adicionado valor unico para code

--- app/models/transaction/record.rb
+++ app/models/transaction/record.rb
@@ -6,4 +6,5 @@ class Transaction::Record < ActiveRecord::Base
   has_many :Item, :class_name => "Transaction::Goods::Item"
   has_one :Xml, :class_name => "Transaction::Xml"
   validates_presence_of :creation_date, :code, :name
+  validates_uniqueness_of :code
 end
