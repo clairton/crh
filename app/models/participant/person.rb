fe57242ccf7482bbe9f43d13f9bf9f55f526b6db
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2011-08-31 23:30:29 -0300

    o campo code da pessoa que armazena o cpf/cnpj deve ser unico

--- app/models/participant/person.rb
+++ app/models/participant/person.rb
@@ -2,5 +2,6 @@ class Participant::Person < ActiveRecord::Base
   has_many :Email, :class_name => "Participant::Contact::Email"
   has_many :Address, :class_name => "Participant::Contact::Address"
   validates_presence_of :first_name,:code
+  validates_uniqueness_of :code
 end
 
