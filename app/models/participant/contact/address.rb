55afbaf9fcf11617422e2cc822b21a7ba6debd31
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2011-08-31 23:33:23 -0300

    cada endereço fisico ou eletronico so pode estar relacionado a uma pessoa

--- app/models/participant/contact/address.rb
+++ app/models/participant/contact/address.rb
@@ -4,4 +4,5 @@ class Participant::Contact::Address < ActiveRecord::Base
   belongs_to :Person, :class_name => "Participant::Person"
   validates_presence_of :participant_person_id
   validates_presence_of :participant_contact_type_id
+  validates_uniqueness_of :address_place_id
 end
--- app/models/participant/contact/email.rb
+++ app/models/participant/contact/email.rb
@@ -4,5 +4,5 @@ class Participant::Contact::Email < ActiveRecord::Base
   belongs_to :Person, :class_name => "Participant::Person"
   validates_presence_of :participant_person_id
   validates_presence_of :participant_contact_type_id
-  validates_uniqueness_of :email_user_id, :scope => :participant_person_id,:scope =>:participant_contact_type_id
+  validates_uniqueness_of :email_user_id
 end
