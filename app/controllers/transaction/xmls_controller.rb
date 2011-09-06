d155e099eacf9f5ba7a7af65dba01c0489d56d24
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2011-09-05 21:08:29 -0300

    colocado a transação tb no controller

--- app/controllers/transaction/xmls_controller.rb
+++ app/controllers/transaction/xmls_controller.rb
@@ -57,17 +57,15 @@ class Transaction::XmlsController < ApplicationController
   # PUT /transaction/xmls/1
   # PUT /transaction/xmls/1.xml
   def update
-    Transaction::Xml.transaction do
-      @transaction_xml = Transaction::Xml.find(params[:id])
-      respond_to do |format|
-        #if @transaction_xml.update_attributes(params[:transaction_xml]) and @transaction_xml.parse(@tempfilefile[7..-2]) 
-        if @transaction_xml.parse('/home/clairton/crh/public/system/xmls/5/original/42110709363232000189550020000020221000012318-nfe.xml') and @transaction_xml.update_attributes(params[:transaction_xml])
-          format.html { redirect_to(@transaction_xml, :notice => 'Xml was successfully updated.') }
-          format.xml  { head :ok }
-        else
-          format.html { render :action => "edit" }
-          format.xml  { render :xml => @transaction_xml.errors, :status => :unprocessable_entity }        
-        end
+    @transaction_xml = Transaction::Xml.find(params[:id])
+    respond_to do |format|
+      #if @transaction_xml.update_attributes(params[:transaction_xml]) and @transaction_xml.parse(@tempfilefile[7..-2]) 
+      if @transaction_xml.parse('/home/clairton/crh/public/system/xmls/5/original/42110709363232000189550020000020221000012318-nfe.xml') and @transaction_xml.update_attributes(params[:transaction_xml])
+        format.html { redirect_to(@transaction_xml, :notice => 'Xml was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @transaction_xml.errors, :status => :unprocessable_entity }        
       end
     end
   end
@@ -75,12 +73,14 @@ class Transaction::XmlsController < ApplicationController
   # DELETE /transaction/xmls/1
   # DELETE /transaction/xmls/1.xml
   def destroy
-    @transaction_xml = Transaction::Xml.find(params[:id])
-    @transaction_xml.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(transaction_xmls_url) }
-      format.xml  { head :ok }
+    Transaction::Xml.transaction do
+      @transaction_xml = Transaction::Xml.find(params[:id])
+      @transaction_xml.destroy
+  
+      respond_to do |format|
+        format.html { redirect_to(transaction_xmls_url) }
+        format.xml  { head :ok }
+      end
     end
   end
 end
