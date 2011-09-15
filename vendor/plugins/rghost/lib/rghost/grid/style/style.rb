98c1bcd0f9f0ac4a8b981117d013c84eff8c1042
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2011-09-15 19:40:06 -0300

    deletado plugins inuteis

--- vendor/plugins/rghost/lib/rghost/grid/style/style.rb
+++
@@ -1,8 +0,0 @@
-
-module RGhost::Grid 
-  module Style
-    require 'rghost/grid/style/border_lines'
-    require 'rghost/grid/style/old_forms'
-    require 'rghost/grid/style/bottom_lines'
-  end
-end
