9165c1feeb6c8f2a9e483d7fdc27b5569b84e8b9
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2011-10-18 22:50:54 -0200

    delecao em cascata atraves do xml

--- app/models/transaction/record.rb
+++ app/models/transaction/record.rb
@@ -4,7 +4,7 @@ class Transaction::Record < ActiveRecord::Base
   has_many :transactionTaxe, :class_name => "Transaction::Taxe", :dependent => :destroy , :foreign_key => "transaction_record_id"
   has_many :transactionTot, :class_name => "Transaction::Tot", :dependent => :destroy , :foreign_key => "transaction_record_id"
   has_many :Item, :class_name => "Transaction::Goods::Item"
-  has_one :Xml, :class_name => "Transaction::Xml"
+  has_one :transactionXml, :class_name => "Transaction::Xml", :dependent => :destroy , :foreign_key => "transaction_record_id"
   validates_presence_of :creation_date, :code, :name
   validates_uniqueness_of :code
 end
