9c5518ace15dc6f7b21ae35f09418d4cdd853560
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2011-10-18 22:01:31 -0200

    arrumado o relaciomento e delete em cascade para o financeiro

--- app/models/transaction/record.rb
+++ app/models/transaction/record.rb
@@ -1,6 +1,6 @@
 class Transaction::Record < ActiveRecord::Base
   has_many :transactionParticipant, :class_name => "Transaction::Participant", :dependent => :destroy, :foreign_key => "transaction_record_id"
-  has_many :Financier, :class_name => "Transaction::Financier"
+  has_many :transactionFinancier, :class_name => "Transaction::Financier", :dependent => :destroy , :foreign_key => "transaction_record_id"
   has_many :Taxe, :class_name => "Transaction::Taxe"
   has_many :Tot, :class_name => "Transaction::Tot"
   has_many :Item, :class_name => "Transaction::Goods::Item"
