2796347381eba0d34795e12acb00acdcdc82eee6
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2011-10-31 22:43:34 -0200

    FIXES erro na chave estrangeira do item

--- app/models/goods/item.rb
+++ app/models/goods/item.rb
@@ -1,5 +1,6 @@
 class Goods::Item < ActiveRecord::Base
-  has_many :transactionItem, :class_name => "Transaction::Goods::Item", :foreign_key => "transaction_goods_item_id", :dependent => :destroy
+  has_many :transactionItem, :class_name => "Transaction::Goods::Item", :foreign_key => "goods_item_id", :dependent => :destroy
   validates_uniqueness_of :code
   validates_presence_of :name, :code
 end
+
